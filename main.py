85b6c9bccc7723a70cd2288c3de7effe163721f4
John Finberg <johnbfinberg@gmail.com>
2021-02-24 23:32:32 -0500

    fix face

--- main.py
+++ main.py
@@ -22,88 +22,92 @@ class FacialRecognition:
         face_names = []
         process_this_frame = True
         recent_faces = {}
+        print('Active scanning')
 
         while True:
             # Grab a single frame of video
             ret, frame = video_capture.read()
 
             # Resize frame of video to 1/4 size for faster face recognition processing
-            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            try:
+                small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-            rgb_small_frame = small_frame[:, :, ::-1]
+                # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+                rgb_small_frame = small_frame[:, :, ::-1]
 
-            # Only process every other frame of video to save time
-            if process_this_frame:
-                # Find all the faces and face encodings in the current frame of video
-                face_locations = face_recognition.face_locations(rgb_small_frame)
-                face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+                # Only process every other frame of video to save time
+                if process_this_frame:
+                    # Find all the faces and face encodings in the current frame of video
+                    face_locations = face_recognition.face_locations(rgb_small_frame)
+                    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-                face_names = []
-                for face_encoding in face_encodings:
-                    # See if the face is a match for the known face(s)
-                    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                    name = "Unknown"
+                    face_names = []
+                    for face_encoding in face_encodings:
+                        # See if the face is a match for the known face(s)
+                        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                        name = "Unknown"
 
-                    # # If a match was found in known_face_encodings, just use the first one.
-                    # if True in matches:
-                    #     first_match_index = matches.index(True)
-                    #     name = known_face_names[first_match_index]
+                        # # If a match was found in known_face_encodings, just use the first one.
+                        # if True in matches:
+                        #     first_match_index = matches.index(True)
+                        #     name = known_face_names[first_match_index]
 
-                    # Or instead, use the known face with the smallest distance to the new face
-                    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                    best_match_index = np.argmin(face_distances)
-                    if matches[best_match_index]:
-                        name = known_face_names[best_match_index]
+                        # Or instead, use the known face with the smallest distance to the new face
+                        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                        best_match_index = np.argmin(face_distances)
+                        if matches[best_match_index]:
+                            name = known_face_names[best_match_index]
 
-                    face_names.append(name)
+                        face_names.append(name)
 
-            process_this_frame = not process_this_frame
-
-
-            # Display the results
-            for (top, right, bottom, left), name in zip(face_locations, face_names):
-                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-                top *= 4
-                right *= 4
-                bottom *= 4
-                left *= 4
-
-                # Draw a box around the face
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-                # Draw a label with a name below the face
-                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-                font = cv2.FONT_HERSHEY_DUPLEX
-                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-            # Display the resulting image (DEBUGGING CAMERA VIEW)
-            # cv2.imshow('Video', frame)
-
-            # Proccess people
-            # TODO: Improve
-            for name in face_names:
-                if name not in recent_faces:
-                    self.process_person(name, frame)
-                    recent_faces[name] = 0
-                elif name in recent_faces:
-                    recent_faces[name] = 0
+                process_this_frame = not process_this_frame
 
 
-            face_names_to_delete = []
-            for name in recent_faces:
-                if recent_faces[name] > 60:
-                    face_names_to_delete.append(name)
-                
-                # detect bodies if bodies = faces stay same
-                hog = cv2.HOGDescriptor()
-                hog.setSVMDetector( cv2.HOGDescriptor_getDefaultPeopleDetector() )
-                found,w=hog.detectMultiScale(frame, winStride=(8,8), padding=(32,32), scale=1.05)
-                if len(found) != len(recent_faces):
-                    recent_faces[name] += 1
+                # Display the results
+                for (top, right, bottom, left), name in zip(face_locations, face_names):
+                    # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+                    top *= 4
+                    right *= 4
+                    bottom *= 4
+                    left *= 4
 
-            for name in face_names_to_delete:
-                del recent_faces[name]
+                    # Draw a box around the face
+                    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+                    # Draw a label with a name below the face
+                    cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                    font = cv2.FONT_HERSHEY_DUPLEX
+                    cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+                # Display the resulting image (DEBUGGING CAMERA VIEW)
+                # cv2.imshow('Video', frame)
+
+                # Proccess people
+                # TODO: Improve
+                for name in face_names:
+                    if name not in recent_faces:
+                        self.process_person(name, frame)
+                        recent_faces[name] = 0
+                    elif name in recent_faces:
+                        recent_faces[name] = 0
+
+
+                face_names_to_delete = []
+                for name in recent_faces:
+                    if recent_faces[name] > 60:
+                        face_names_to_delete.append(name)
+                    
+                    # detect bodies if bodies = faces stay same
+                    hog = cv2.HOGDescriptor()
+                    hog.setSVMDetector( cv2.HOGDescriptor_getDefaultPeopleDetector() )
+                    found,w=hog.detectMultiScale(frame, winStride=(8,8), padding=(32,32), scale=1.05)
+                    if len(found) != len(recent_faces):
+                        recent_faces[name] += 1
+
+                for name in face_names_to_delete:
+                    del recent_faces[name]
+            except Exception as e:
+                print(str(e))
 
             # Hit 'q' on the keyboard to quit!
             if cv2.waitKey(1) & 0xFF == ord('q'):
